0641ec59ac74c2dab9dc9df9cd5e8f54e788d00a
WitherVictor <number42@yeah.net>
2025-02-14 11:02:17 +0800

    修改测试用例名

--- unit-tests/sqrt_newton_unittest.cpp
+++ unit-tests/sqrt_newton_unittest.cpp
@@ -4,15 +4,22 @@
 
 #include "sqrt_newton.hpp"
 
-TEST(sqrt_newton_unittest, extreme_positive_input) {
+TEST(sqrt_newton_unittest, extreme_positive) {
     EXPECT_DOUBLE_EQ(sqrt_newton(1e30), 1e15);
 }
 
-TEST(sqrt_newton_unittest, golden_ratio_input) {
+TEST(sqrt_newton_unittest, golden_ratio) {
     EXPECT_DOUBLE_EQ(((1 + sqrt_newton(5)) / 2), 1.618033988749895);
 }
 
-TEST(sqrt_newton_unittest, normal_input) {
+TEST(sqrt_newton_unittest, decimal) {
+    EXPECT_DOUBLE_EQ(sqrt_newton(0.5), 0.7071067811865476);
+    EXPECT_DOUBLE_EQ(sqrt_newton(0.25), 0.5);
+    EXPECT_DOUBLE_EQ(sqrt_newton(0.125), 0.3535533905932738);
+    
+}
+
+TEST(sqrt_newton_unittest, trivial) {
     EXPECT_DOUBLE_EQ(sqrt_newton(1), 1.0);
     EXPECT_DOUBLE_EQ(sqrt_newton(2), 1.4142135623730951);
     EXPECT_DOUBLE_EQ(sqrt_newton(3), 1.7320508075688772);
@@ -24,6 +31,6 @@ TEST(sqrt_newton_unittest, zero) {
     EXPECT_DOUBLE_EQ(sqrt_newton(0), 0);
 }
 
-TEST(sqrt_newton_unittest, negative_input) {
+TEST(sqrt_newton_unittest, negative) {
     EXPECT_TRUE(std::isnan(sqrt_newton(-1)));
 }
