e2b4563c48084ac84f622fae356bd6560adce4a0
WitherVictor <number42@yeah.net>
2025-02-19 10:01:02 +0800

    实现线程池

---
+++ include/thread_pool.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <concepts>
+#include <functional>
+#include <future>
+#include <memory>
+#include <mutex>
+#include <stop_token>
+#include <thread>
+#include <queue>
+#include <condition_variable>
+
+class thread_pool {
+public:
+    thread_pool(std::size_t thread_count = std::thread::hardware_concurrency())
+        : m_queue_mutex{}, m_queue_cv{}, m_queue{}, m_threads(thread_count) 
+    {
+        for (auto& thread : m_threads) {
+            thread = std::jthread([this](std::stop_token stop_token) {
+                while (true) {
+                    std::unique_lock lock{m_queue_mutex};
+
+                    //  存在谓词的 wait() 在谓词返回 true 时直接跳过阻塞
+                    //  因此线程池析构时队列若存在多个任务，不会导致死锁
+                    m_queue_cv.wait(lock, [this, &stop_token]() {
+                        return !m_queue.empty() || stop_token.stop_requested(); });
+
+                    if (stop_token.stop_requested() && m_queue.empty())
+                        return;
+
+                    auto task = std::move(m_queue.front());
+                    m_queue.pop();
+                    lock.unlock();
+                    
+                    task();
+                }
+            });
+        }
+    }
+
+    ~thread_pool() {
+        for (auto& thread : m_threads)
+            thread.request_stop();
+
+        m_queue_cv.notify_all();
+    }
+
+    template <typename Func, typename... Args>
+    requires std::invocable<Func, Args...>
+    auto submit(Func&& func, Args&&... args) {
+        using return_type = std::invoke_result_t<Func, Args...>;
+
+        //  任务队列的元素类型 std::function<void()>，需要可复制构造
+        //  std::unique_ptr 只可移动不可复制，因此使用 std::shared_ptr 包装任务
+        auto task = std::make_shared<std::packaged_task<return_type()>>(
+            [func = std::forward<Func>(func), ...args = std::forward<Args>(args)]() mutable {
+                return std::invoke(func, args...);
+            }
+        );
+
+        auto result_future = task->get_future();
+
+        std::unique_lock lock{m_queue_mutex};
+        m_queue.emplace([task = std::move(task)]() mutable { (*task)(); });
+        lock.unlock();
+
+        m_queue_cv.notify_one();
+        
+        return result_future;
+    }
+
+private:
+    mutable std::mutex m_queue_mutex;
+    std::condition_variable m_queue_cv;
+    std::queue<std::function<void()>> m_queue;
+
+    //  在通知工作所有线程后，析构函数体执行完毕
+    //  接下来会以声明的逆序销毁成员变量
+    //  而析构 jthread 会自动调用 join()，因此无需手动 join()
+    std::vector<std::jthread> m_threads;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,33 @@
 #include "sqrt_newton.hpp"
 #include "roll_dice.hpp"
 #include "timer.hpp"
+#include "thread_pool.hpp"
+
+std::size_t calculate_add() {
+    std::size_t result = 0;
+    for (std::size_t i = 0; i < 100000; i++)
+        result += i;
+
+    return result;
+}
+
+std::size_t calculate_multiply() {
+    std::size_t result = 1;
+    for (std::size_t i = 1; i < 25; i++)
+        result *= i;
+
+    return result;
+}
 
 int main() {
     ::timer timer{};
     std::println("{}", roll_dice("6d6"));
     std::println("{}", sqrt_newton(3));
     std::println("Time elapsed : {}", timer.now());
+
+    thread_pool pool{};
+    auto add_future = pool.submit(calculate_add);
+    auto multiply_future = pool.submit(calculate_multiply);
+    std::println("Add result : {}", add_future.get());
+    std::println("Multiply result : {}", multiply_future.get());
 }
