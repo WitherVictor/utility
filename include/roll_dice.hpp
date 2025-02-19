5355a316421cf2aec76c7506b7002ecf04eaec67
WitherVictor <number42@yeah.net>
2025-02-19 10:13:58 +0800

    将 rng 分离为单独的头文件

--- include/roll_dice.hpp
+++ include/roll_dice.hpp
@@ -10,29 +10,7 @@
 
 //  ranges formatter for std::vector<T>
 #include "format_ranges.hpp"
-
-template <std::integral Tp>
-class rng {
-public:
-    rng(Tp begin, Tp end)
-        : m_range{begin, end}
-    {
-        std::random_device rd{};
-        std::array<Tp, std::mt19937_64::state_size> seed_data{};
-        std::ranges::generate(seed_data, std::ref(rd));
-        std::seed_seq seed_sequence(std::begin(seed_data), std::end(seed_data));
-
-        m_engine = std::mt19937_64{seed_sequence};
-    }
-
-    Tp yield() { return m_range(m_engine); }
-private:
-    std::mt19937_64 m_engine;
-    std::uniform_int_distribution<Tp> m_range;
-};
-
-template <typename Tp, typename Up>
-rng(Tp, Up) -> rng<std::common_type_t<Tp, Up>>;
+#include "rng.hpp"
 
 struct dice_data {
     std::size_t count{};
