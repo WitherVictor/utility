bf92c7082516299bf821d7281e7674b0bc5b2042
WitherVictor <number42@yeah.net>
2025-02-20 17:49:58 +0800

    为 rng 添加单元测试

---
+++ unit-tests/rng_unittest.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+
+#include "rng.hpp"
+
+TEST(rng_unittest, basic_test) {
+    rng r1{0, 100};
+    auto v1 = r1();
+
+    EXPECT_GE(v1, 0);
+    EXPECT_LT(v1, 100);
+}
+
+TEST(rng_unittest, exception_test) {
+    EXPECT_NO_THROW(rng(0, 100));
+    EXPECT_THROW(rng(100, 0), std::invalid_argument);
+    EXPECT_THROW(rng(20, 20), std::invalid_argument);
+}
