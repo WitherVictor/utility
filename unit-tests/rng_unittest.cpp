97420b80edcdae778186716a20d60b796549f660
WitherVictor <number42@yeah.net>
2025-02-21 08:53:30 +0800

    修改了测试文件，匹配整数类型随机数生成器的值域

--- unit-tests/rng_unittest.cpp
+++ unit-tests/rng_unittest.cpp
@@ -12,6 +12,7 @@ TEST(rng_unittest, basic_test) {
 
 TEST(rng_unittest, exception_test) {
     EXPECT_NO_THROW(rng(0, 100));
+    EXPECT_NO_THROW(rng(20, 20));
+
     EXPECT_THROW(rng(100, 0), std::invalid_argument);
-    EXPECT_THROW(rng(20, 20), std::invalid_argument);
 }
