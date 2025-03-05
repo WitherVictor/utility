c92cbea817f99dd97674f2c12b1ddb614122fb58
WitherVictor <number42@yeah.net>
2025-03-05 14:29:53 +0800

    为 gcd_unittest 添加负数参数测试

--- unit-tests/gcd_unittest.cpp
+++ unit-tests/gcd_unittest.cpp
@@ -8,6 +8,12 @@ TEST(gcd_unittest, basic_test) {
     EXPECT_EQ(gcd(600, 125), 25);
 }
 
+TEST(gcd_unittest, negative_test) {
+    EXPECT_EQ(gcd(-12, -18), 6);
+    EXPECT_EQ(gcd(24, -36), 6);
+    EXPECT_EQ(gcd(-44, 28), 4);
+}
+
 TEST(gcd_unittest, one_arg_zero) {
     EXPECT_EQ(gcd(42, 0), 42);
     EXPECT_EQ(gcd(0, 72), 72);
