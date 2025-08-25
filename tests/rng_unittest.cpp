055b75e0a18729415388d3ac0df38d073005ca6a
WitherVictor <number42@yeah.net>
2025-08-25 10:07:47 +0800

    重命名文件夹

--- tests/rng_unittest.cpp
+++ tests/rng_unittest.cpp
@@ -2,12 +2,14 @@
 
 #include "rng.hpp"
 
-TEST(rng_unittest, basic_test) {
-    rng r1{0, 100};
-    auto v1 = r1();
+TEST(rng_unittest, integer_test) {
+    auto r1{rng(0, 100)};
+    for (const auto _ : std::views::iota(0, 100)) {
+        auto val{r1()};
 
-    EXPECT_GE(v1, 0);
-    EXPECT_LT(v1, 100);
+        EXPECT_GE(val, 0);
+        EXPECT_LE(val, 100);
+    }
 }
 
 TEST(rng_unittest, exception_test) {
