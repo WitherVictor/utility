ca51fafd7aff220578be6fec6644970139bc93ea
WitherVictor <number42@yeah.net>
2025-08-25 10:00:35 +0800

    修改 rng_coro 的实现

--- include/rng_coro.hpp
+++ include/rng_coro.hpp
@@ -1,67 +1,24 @@
 #include <algorithm>
 #include <array>
-#include <coroutine>
-#include <exception>
+#include <stdexcept>
 #include <random>
 #include <stdexcept>
 #include <type_traits>
-
-#if __cplusplus < 202302L || !__cpp_lib_generator
-
-template <typename T>
-struct generator {
-    struct promise_type {
-        auto initial_suspend() { return std::suspend_always{}; }
-        auto final_suspend() noexcept { return std::suspend_never{}; }
-        auto return_void() {}
-        auto unhandled_exception() { return std::terminate(); }
-        auto get_return_object() {
-            return generator{
-                std::coroutine_handle<promise_type>::from_promise(*this)
-            };
-        }
-
-        auto yield_value(T&& value) {
-            current_value = value;
-            return std::suspend_always{};
-        }
-
-        T current_value;
-    };
-
-    std::coroutine_handle<promise_type> handle;
-
-    auto yield() {
-        handle.resume();
-        return handle.promise().current_value;
-    }
-};
-
-#else
-
 #include <generator>
-
-#endif
 
 template <typename T, typename engine_type = std::mt19937_64>
 requires std::is_arithmetic_v<T>
-#if __cplusplus < 202302L || !__cpp_lib_generator
-generator<T>
-#else
-std::generator<T>
-#endif
-rng(T left, T right) {
+std::generator<T> rng(T left, T right) {
     if (left > right)
         throw std::invalid_argument{ "The left bound of rng should be smaller than right bound."};
 
     //  generate random seeds
     std::random_device rd{};
     std::array<std::random_device::result_type, engine_type::state_size> seed_data{};
-    std::ranges::generate(seed_data, std::ref(rd));
+    std::ranges::generate(seed_data, [&rd]() { return rd(); });
 
-    //  create generator engine depends on seed sequence
-    std::seed_seq seed_sequence(seed_data.begin(), seed_data.end());
-    engine_type engine{seed_sequence};
+    std::seed_seq seeds{seed_data.begin(), seed_data.end()};
+    engine_type engine{seeds};
 
     //  generate range according to type T is integer or floating point
     auto range = [&left, &right]() {
