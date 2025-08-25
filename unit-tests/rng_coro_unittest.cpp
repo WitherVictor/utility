643c28c76cc7190ad7d18ea2a6afc2bc1a5c4cdf
WitherVictor <number42@yeah.net>
2025-08-25 10:00:45 +0800

    添加 rng_coro 的单元测试

---
+++ unit-tests/rng_coro_unittest.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include <ranges>
+//  Target test file
+#include <rng_coro.hpp>
+#include <stdexcept>
+
+TEST(rng_coro_unittest, integer_test) {
+    for (const auto& value : rng(0, 100) | std::views::take(1000)) {
+        EXPECT_GE(value, 0);
+        EXPECT_LE(value, 100);
+    }
+}
+
+TEST(rng_coro_unittest, float_test) {
+    for (const auto& value : rng(0.0f, 1.0f) | std::views::take(1000)) {
+        EXPECT_GE(value, 0.0f);
+        EXPECT_LE(value, 1.0f);
+    }
+}
+
+TEST(rng_coro_unittest, exception_test) {
+    EXPECT_THROW(for (const auto& _ : rng(100, 0) | std::views::take(1000)) {}, std::invalid_argument);
+    EXPECT_THROW(for (const auto& _ : rng(100.0, 0.0) | std::views::take(1000)) {}, std::invalid_argument);
+}
