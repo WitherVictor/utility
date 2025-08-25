775301c42195fefd92c8e270b03bfcf1f4b4be94
WitherVictor <number42@yeah.net>
2025-08-25 10:16:18 +0800

    添加浮点数单元测试

--- tests/rng_unittest.cpp
+++ tests/rng_unittest.cpp
@@ -12,6 +12,16 @@ TEST(rng_unittest, integer_test) {
     }
 }
 
+TEST(rng_unittest, float_test) {
+    auto r1{rng(0.0, 100.0)};
+    for (const auto _ : std::views::iota(0, 100)) {
+        auto val{r1()};
+
+        EXPECT_GE(val, 0.0);
+        EXPECT_LE(val, 100.0);
+    }
+}
+
 TEST(rng_unittest, exception_test) {
     EXPECT_NO_THROW(rng(0, 100));
     EXPECT_NO_THROW(rng(20, 20));
